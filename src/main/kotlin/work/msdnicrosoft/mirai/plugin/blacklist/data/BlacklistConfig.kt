8491572209321658f22343a70930ca9349224e9e
MSDNicrosoft <wang3311835119@hotmail.com>
2023-12-10 09:10:27 +0800

    style: Reformat code(Fix detekt)
    Signed-off-by: MSDNicrosoft <wang3311835119@hotmail.com>

--- src/main/kotlin/work/msdnicrosoft/mirai/plugin/blacklist/data/BlacklistConfig.kt
+++ src/main/kotlin/work/msdnicrosoft/mirai/plugin/blacklist/data/BlacklistConfig.kt
@@ -4,7 +4,7 @@ import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.value
 
-object BlacklistConfig: AutoSavePluginConfig("blacklist") {
+object BlacklistConfig : AutoSavePluginConfig("blacklist") {
 
     @ValueDescription("是否启用黑名单")
     var enabled by value<Boolean>(false)
